69aa003e73d20329524b7a0c7212c9b01f59eda7
rafal_ubuntu_praca <rafal@gmail>
2019-03-03 23:39:13 +0100

    request json

--- src/main/java/dialoflow/mongo/MongoClientTestowy.java
+++ src/main/java/dialoflow/mongo/MongoClientTestowy.java
@@ -19,6 +19,79 @@ import com.mongodb.client.MongoDatabase;
 @Service
 public class MongoClientTestowy {
 
+	private static final String request="{\n" + 
+			"   \"responseId\": \"c943d1e2-b190-4bcc-b8b3-66379e3faeb5\",\n" + 
+			"   \"queryResult\": {\n" + 
+			"     \"queryText\": \"much\",\n" + 
+			"     \"parameters\": {\n" + 
+			"       \"amount\": \"medium\",\n" + 
+			"       \"toilet_activity\": \"piss\"\n" + 
+			"     },\n" + 
+			"     \"allRequiredParamsPresent\": true,\n" + 
+			"     \"fulfillmentText\": \"Good job! You did medium piss. Do you want to share more info about you toilet activity?\",\n" + 
+			"     \"fulfillmentMessages\": [{\n" + 
+			"       \"text\": {\n" + 
+			"         \"text\": [\"Good job! You did medium piss. Do you want to share more info about you toilet activity?\"]\n" + 
+			"       }\n" + 
+			"     }],\n" + 
+			"     \"outputContexts\": [{\n" + 
+			"       \"name\": \"projects/pysior/agent/sessions/babd77a7-789d-4d81-84ab-7e02308532fb/contexts/toilet-followup\",\n" + 
+			"       \"lifespanCount\": 2,\n" + 
+			"       \"parameters\": {\n" + 
+			"         \"amount\": \"medium\",\n" + 
+			"         \"toilet_activity.original\": \"pee\",\n" + 
+			"         \"toilet_activity\": \"piss\",\n" + 
+			"         \"amount.original\": \"much\"\n" + 
+			"       }\n" + 
+			"     }, {\n" + 
+			"       \"name\": \"projects/pysior/agent/sessions/babd77a7-789d-4d81-84ab-7e02308532fb/contexts/toilet-followup-2\",\n" + 
+			"       \"lifespanCount\": 2,\n" + 
+			"       \"parameters\": {\n" + 
+			"         \"amount\": \"medium\",\n" + 
+			"         \"toilet_activity.original\": \"pee\",\n" + 
+			"         \"toilet_activity\": \"piss\",\n" + 
+			"         \"amount.original\": \"much\"\n" + 
+			"       }\n" + 
+			"     }, {\n" + 
+			"       \"name\": \"projects/pysior/agent/sessions/babd77a7-789d-4d81-84ab-7e02308532fb/contexts/generic\",\n" + 
+			"       \"lifespanCount\": 4,\n" + 
+			"       \"parameters\": {\n" + 
+			"         \"slack_user_id\": \"UF70LU93K\",\n" + 
+			"         \"amount\": \"medium\",\n" + 
+			"         \"slack_channel\": \"DF7QLR8JH\",\n" + 
+			"         \"toilet_activity.original\": \"pee\",\n" + 
+			"         \"toilet_activity\": \"piss\",\n" + 
+			"         \"amount.original\": \"much\"\n" + 
+			"       }\n" + 
+			"     }],\n" + 
+			"     \"intent\": {\n" + 
+			"       \"name\": \"projects/pysior/agent/intents/cb8fae01-09fd-416e-8976-1d444b125d38\",\n" + 
+			"       \"displayName\": \"toilet\"\n" + 
+			"     },\n" + 
+			"     \"intentDetectionConfidence\": 1.0,\n" + 
+			"     \"languageCode\": \"en\"\n" + 
+			"   },\n" + 
+			"   \"originalDetectIntentRequest\": {\n" + 
+			"     \"source\": \"slack_testbot\",\n" + 
+			"     \"payload\": {\n" + 
+			"       \"data\": {\n" + 
+			"         \"client_msg_id\": \"e823fa5a-aa06-4644-8e6f-18a37c54dc78\",\n" + 
+			"         \"event_ts\": \"1548341364.003500\",\n" + 
+			"         \"channel\": \"DF7QLR8JH\",\n" + 
+			"         \"text\": \"much\",\n" + 
+			"         \"channel_type\": \"im\",\n" + 
+			"         \"type\": \"message\",\n" + 
+			"         \"user\": \"UF70LU93K\",\n" + 
+			"         \"ts\": \"1548341364.003500\"\n" + 
+			"       },\n" + 
+			"       \"source\": \"slack_testbot\"\n" + 
+			"     }\n" + 
+			"   },\n" + 
+			"   \"session\": \"projects/pysior/agent/sessions/babd77a7-789d-4d81-84ab-7e02308532fb\"\n" + 
+			" }\n" + 
+			"";
+	
+	
 	public void getAllCities() {
 		MongoClient mongoClient = MongoClients.create();
 		 MongoDatabase database = mongoClient.getDatabase("test_1234");
@@ -32,6 +105,10 @@ public class MongoClientTestowy {
 		     cursor.close();
 		 }
 		 mongoClient.close();
+	}
+	
+	public void saveDFRequest() {
+		
 	}
 	
 }
